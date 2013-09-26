b11d1c8e2c7ba7fb71451a5f9a2940d9723fd5a1
Michael Smith <michael.smith6@mail.mcgill.ca>
2013-09-26 00:00:54 -0400

    Caching working except for the free/malloc in create_cached_block

--- pa1/caching.c
+++ pa1/caching.c
@@ -4,16 +4,16 @@
 #include "mydisk.h"
 
 /* The cache entry struct */
-struct cache_entry
-{
-	int block_id;
-	int is_dirty;
-	char content[BLOCK_SIZE];
+struct cache_entry {
+    int block_id;
+    int is_dirty;
+    char content[BLOCK_SIZE];
 };
 
-int cache_blocks;  /* number of blocks for the cache buffer */
+int cache_blocks; /* number of blocks for the cache buffer */
 struct cache_entry *ring_buffer;
 int cache_size;
+int next_insert_pos;
 
 /**
  * This function will return the index the given block has been given in the
@@ -23,62 +23,110 @@ int cache_size;
 int find_cached_entry(int block_id);
 
 
-int init_cache(int nblocks)
-{
-	/* TODO: allocate proper data structure (ring buffer)
-	 * initialize entry data so that the the ring buffer is empty
-	 */
-        cache_blocks = nblocks;
-        ring_buffer = malloc(cache_blocks * sizeof(cache_entry));
-        cache_size = 0;
-        cache_enabled = 1;
-	return 0;
+int init_cache(int nblocks) {
+    /* TODO: allocate proper data structure (ring buffer)
+     * initialize entry data so that the the ring buffer is empty
+     */
+    cache_blocks = nblocks;
+    ring_buffer = malloc(cache_blocks * sizeof(struct cache_entry));
+    int i;
+    // Initialize ring data
+    for (i = 0; i < cache_blocks; ++i) {
+        ring_buffer[i].block_id = -1;
+        ring_buffer[i].is_dirty = 0;
+    }
+    cache_size = 0;
+    cache_enabled = 1;
+    next_insert_pos = 0;
+    return 0;
 }
 
-int close_cache()
-{
-        /* TODO: release the memory for the ring buffer */
-        free(ring_buffer);
+int close_cache() {
+    // Write dirty data
+    int i;
+    
+    for (i = 0; i < cache_size; ++i) {
+        struct cache_entry *entry = &ring_buffer[i];
+        if (entry->is_dirty == 1) {
+            mydisk_write_block(entry->block_id, entry->content);
+        }
+    }
+    
+    // Free memory, reset variables
+    free(ring_buffer);
+    cache_enabled = 0;
+    cache_size = 0;
+    return 0;
+}
+
+void *get_cached_block(int block_id) {
+    // Get entry, if -1, entry not found, else return content
+    int cached_entry_pos = find_cached_entry(block_id);
+
+    if (cached_entry_pos == -1) {
+        return NULL;
+    }
+
+    else {
+        return ring_buffer[cached_entry_pos].content;
+    }
+}
+
+void *create_cached_block(int block_id) {
+    /* TODO: create a new entry, insert it into the ring buffer
+     * It might kick an exisitng entry.
+     * Remember to write dirty block back to disk
+     * Note that: think if you can use mydisk_write_block() to 
+     * flush dirty blocks to disk
+     */
+    // Entry already exists, return position
+    int existing_entry = find_cached_entry(block_id);
+    if (existing_entry != -1) {
+        return ring_buffer[existing_entry].content;
+    }
+    
+    // Existing entry in cache - if dirty, we write it to disk
+    // It could be that there is actually no entry, but init sets is_dirty
+    // to 0 in that case
+    struct cache_entry *entry = &ring_buffer[next_insert_pos];
+    int orig_block_id = entry->block_id;
+    if (entry->is_dirty == 1) {
+        // Write to disk (cache must be disabled to actually write to disk)
         cache_enabled = 0;
-        cache_size = 0;
-        return 0;
+        mydisk_write_block(orig_block_id,entry->content);
+        cache_enabled = 1;
+    }
+    // Now that a dirty block (if any) has been taken care of, we write
+    // (or overwrite) an entry
+//    free(entry->content);
+//    entry = malloc(sizeof(struct cache_entry));
+    entry->block_id = block_id;
+    entry->is_dirty = 0;
+    // Generally, next position to insert is the next in sequence
+    // Except at the end, where we go back to 0
+    next_insert_pos++;
+    if (next_insert_pos == (cache_blocks -1 )) {
+        next_insert_pos = 0;
+    }
+    if (orig_block_id == -1) {
+        cache_size++;
+    }
+    return entry->content;
 }
 
-void *get_cached_block(int block_id)
-{
-        // Get entry, if -1, entry not found, else return content
-        int cached_entry_pos = find_cached_entry(block_id);
-        if (cached_entry_pos == -1){
-            return NULL;
-        }
-        else {
-            return ring_buffer[cached_entry_pos].content;
-        }
+void mark_dirty(int block_id) {
+    int entry = find_cached_entry(block_id);
+    // We assume the entry exists
+    ring_buffer[entry].is_dirty = 1;
 }
 
-void *create_cached_block(int block_id)
-{
-	/* TODO: create a new entry, insert it into the ring buffer
-	 * It might kick an exisitng entry.
-	 * Remember to write dirty block back to disk
-	 * Note that: think if you can use mydisk_write_block() to 
-	 * flush dirty blocks to disk
-	 */
-	return NULL;
-}
-
-void mark_dirty(int block_id)
-{
-	/* TODO: find the entry and mark it dirty */
-}
-
-int find_cached_entry(int block_id){
+int find_cached_entry(int block_id) {
     int i;
     /*
      * We examine all entries until we get a match
      */
-    for (i = 0; i < cache_size; ++i){
-        if (ring_buffer[i].block_id == block_id){
+    for (i = 0; i < cache_size; ++i) {
+        if (ring_buffer[i].block_id == block_id) {
             return i;
         }
     }
--- pa1/mydisk.c
+++ pa1/mydisk.c
@@ -2,198 +2,237 @@
 #include <string.h>
 #include <stdio.h>
 
-FILE *thefile;     /* the file that stores all blocks */
-int max_blocks;    /* max number of blocks given at initialization */
-int disk_type;     /* disk type, 0 for HDD and 1 for SSD */
+FILE *thefile; /* the file that stores all blocks */
+int max_blocks; /* max number of blocks given at initialization */
+int disk_type; /* disk type, 0 for HDD and 1 for SSD */
 int cache_enabled; /* is cache enabled? 0 no, 1 yes */
 
-int mydisk_init(char const *file_name, int nblocks, int type)
-{
-	thefile = fopen(file_name, "wb+");
-	char zero[] = "\0";
-	int num_to_write = BLOCK_SIZE * nblocks;
-	int num_written = 0;
-	int counter = 0;
-	while (counter < num_to_write){
-		num_written += fwrite(zero, 1, 1, thefile);
-		counter++;
-	}
-	if (num_written != num_to_write){
-		return 1;
-	}
-	max_blocks = nblocks;
-	disk_type = type;		
-	return 0;
+int mydisk_init(char const *file_name, int nblocks, int type) {
+    
+    thefile = fopen(file_name, "wb+");
+    char zero[] = "\0";
+    int num_to_write = BLOCK_SIZE * nblocks;
+    int num_written = 0;
+    int counter = 0;
+    
+    while (counter < num_to_write) {
+        num_written += fwrite(zero, 1, 1, thefile);
+        counter++;
+    }
+    
+    if (num_written != num_to_write) {
+        return 1;
+    }
+    
+    max_blocks = nblocks;
+    disk_type = type;
+    return 0;
 }
 
-void mydisk_close()			
-{
-	fclose(thefile);
+void mydisk_close() {
+    fclose(thefile);
 }
 
-int mydisk_read_block(int block_id, void *buffer)
-{
-	if (block_id > max_blocks){
-		return 1;
-	}
-	
-	if (cache_enabled) {
-		/* TODO: 1. check if the block is cached
-		 * 2. if not create a new entry for the block and read from disk
-		 * 3. fill the requested buffer with the data in the entry 
-		 * 4. return proper return code
-		 */
-		 
-		return 0;
-	} else {
-		/* TODO: use standard C functiosn to read from disk
-		 */
-		 //Seek to position
-		 int seek_success = fseek(thefile, block_id * BLOCK_SIZE, SEEK_SET);
-		 if (seek_success == -1){
-		 	// Error
-		 	return 1;
-		 }
-		 
-		 // Read file
-		 int num_read = fread(buffer, 1, BLOCK_SIZE, thefile);
-		 if (num_read != BLOCK_SIZE){
-		 	return 1;
-		 }
-		return 0;
-	}
+int mydisk_read_block(int block_id, void *buffer) {
+    if (block_id > max_blocks) {
+        return 1;
+    }
+
+    if (cache_enabled) {
+
+        /* TODO: 1. check if the block is cached
+         * 2. if not create a new entry for the block and read from disk
+         * 3. fill the requested buffer with the data in the entry 
+         * 4. return proper return code 
+         */
+        void *cache = get_cached_block(block_id);
+        // Cache miss
+        if (cache == NULL) {
+            cache = create_cached_block(block_id);
+            
+            //Seek to position
+            int seek_success = fseek(thefile, block_id * BLOCK_SIZE, SEEK_SET);
+            if (seek_success == -1) {
+                // Error
+                return 1;
+            }
+            
+            // Read file
+            int num_read = fread(buffer, 1, BLOCK_SIZE, thefile);
+            if (num_read != BLOCK_SIZE) {
+                return 1;
+            }
+            
+            // Copy to cache
+            memcpy(cache, buffer, BLOCK_SIZE);
+        }
+        // Cache hit
+        else {
+            memcpy(buffer,cache,BLOCK_SIZE);
+        }
+        return 0;
+    } 
+    
+    else {
+        /* TODO: use standard C functiosn to read from disk
+         */
+        //Seek to position
+        int seek_success = fseek(thefile, block_id * BLOCK_SIZE, SEEK_SET);
+        if (seek_success == -1) {
+            // Error
+            return 1;
+        }
+
+        // Read file
+        int num_read = fread(buffer, 1, BLOCK_SIZE, thefile);
+        if (num_read != BLOCK_SIZE) {
+            return 1;
+        }
+        return 0;
+    }
 }
 
-int mydisk_write_block(int block_id, void *buffer)
-{
-	/* TODO: this one is similar to read_block() except that
-	 * you need to mark it dirty
-	 */
-	 
-	 /*
-	 * TODO: MARK DIRTY! */
-	 if (block_id > max_blocks){
-	 	return 1;
-	 }
-	 
-	 //Seek to position
-	int seek_success = fseek(thefile, block_id * BLOCK_SIZE, SEEK_SET);
-	if (seek_success == -1){
-		// Error
-		return 1;
-	}
-	// Write file
-	int num_written = fwrite(buffer, 1, BLOCK_SIZE, thefile);
-	if (num_written != BLOCK_SIZE){
-		return 1;
-	}
-	
-	return 0;
+int mydisk_write_block(int block_id, void *buffer) {
+    /* TODO: this one is similar to read_block() except that
+     * you need to mark it dirty
+     */
+
+    if (block_id > max_blocks) {
+        return 1;
+    }
+
+    if (cache_enabled) {
+        void *cache = get_cached_block(block_id);
+        // Cache miss
+        if (cache == NULL) {
+            cache = create_cached_block(block_id);
+            // Now we simply dump the buffer into the cache and mark as dirty
+            memcpy(cache, buffer, BLOCK_SIZE);
+            mark_dirty(block_id);
+        }
+        // Cache hit
+        else {
+            // Simply replace existing content, mark dirty
+            memcpy(cache, buffer, BLOCK_SIZE);
+            mark_dirty(block_id);
+        }
+    }
+    
+    else {
+        //Seek to position
+        int seek_success = fseek(thefile, block_id * BLOCK_SIZE, SEEK_SET);
+        if (seek_success == -1) {
+            // Error
+            return 1;
+        }
+        // Write file
+        int num_written = fwrite(buffer, 1, BLOCK_SIZE, thefile);
+        if (num_written != BLOCK_SIZE) {
+            return 1;
+        }
+    }
+
+    return 0;
 }
 
-int mydisk_read(int start_address, int nbytes, void *buffer)
-{
-	int offset, remaining, amount, block_id;
-	int cache_hit = 0, cache_miss = 0;
-	
-	// Parameter check
-	if ((start_address < 0) || (start_address + nbytes < start_address) || (start_address + nbytes >= (max_blocks * BLOCK_SIZE))){
-		return 1;
-	}
-	
-	// Init needed variables
-	block_id = start_address / BLOCK_SIZE;
-	remaining = nbytes;
-	offset = start_address % BLOCK_SIZE;
-	char *temp_buffer = malloc(BLOCK_SIZE);
-	
-	int bytes_completed = 0;
-	while( remaining != 0){
-		// Want to read all of this block or more
-		if ((remaining + offset) >= BLOCK_SIZE){
-			amount = BLOCK_SIZE - offset;
-		}
-		// Want to read only part of this block
-		else{
-			amount = remaining;
-		}
-		//Get block, copy
-		mydisk_read_block(block_id,temp_buffer);
-		memcpy(buffer + bytes_completed, &temp_buffer[offset], amount);
-		// Update counters
-		bytes_completed += amount;
-		remaining -= amount;
-		block_id++;
-		// Offset only useful for first block
-		offset = 0;
-	}
-	free(temp_buffer);
-	
-	/* TODO: 1. first, always check the parameters
-	 * 2. a loop which process one block each time
-	 * 2.1 offset means the in-block offset
-	 * amount means the number of bytes to be moved from the block
-	 * (starting from offset)
-	 * remaining means the remaining bytes before final completion
-	 * 2.2 get one block, copy the proper portion
-	 * 2.3 update offset, amount and remaining
-	 * in terms of latency calculation, monitor if cache hit/miss
-	 * for each block access
-	 */
-	return 0;
+int mydisk_read(int start_address, int nbytes, void *buffer) {
+    int offset, remaining, amount, block_id;
+    int cache_hit = 0, cache_miss = 0;
+
+    // Parameter check
+    if ((start_address < 0) || (start_address + nbytes < start_address) || (start_address + nbytes >= (max_blocks * BLOCK_SIZE))) {
+        return 1;
+    }
+
+    // Init needed variables
+    block_id = start_address / BLOCK_SIZE;
+    remaining = nbytes;
+    offset = start_address % BLOCK_SIZE;
+    char *temp_buffer = malloc(BLOCK_SIZE);
+
+    int bytes_completed = 0;
+    while (remaining != 0) {
+        // Want to read all of this block or more
+        if ((remaining + offset) >= BLOCK_SIZE) {
+            amount = BLOCK_SIZE - offset;
+        }            // Want to read only part of this block
+        else {
+            amount = remaining;
+        }
+        //Get block, copy
+        mydisk_read_block(block_id, temp_buffer);
+        memcpy(buffer + bytes_completed, &temp_buffer[offset], amount);
+        // Update counters
+        bytes_completed += amount;
+        remaining -= amount;
+        block_id++;
+        // Offset only useful for first block
+        offset = 0;
+    }
+    free(temp_buffer);
+
+    /* TODO: 1. first, always check the parameters
+     * 2. a loop which process one block each time
+     * 2.1 offset means the in-block offset
+     * amount means the number of bytes to be moved from the block
+     * (starting from offset)
+     * remaining means the remaining bytes before final completion
+     * 2.2 get one block, copy the proper portion
+     * 2.3 update offset, amount and remaining
+     * in terms of latency calculation, monitor if cache hit/miss
+     * for each block access
+     */
+    return 0;
 }
 
-int mydisk_write(int start_address, int nbytes, void *buffer)
-{
-	/* TODO: similar to read, except the partial write problem
-	 * When a block is modified partially, you need to first read the block,
-	 * modify the portion and then write the whole block back
-	 */
-	int offset, remaining, amount, block_id;
-	int cache_hit = 0, cache_miss = 0;
-	
-	// Parameter check
-	if ((start_address < 0) || (start_address + nbytes < start_address) || (start_address + nbytes >= (max_blocks * BLOCK_SIZE))){
-		return 1;
-	}
-	
-	// Init needed variables
-	block_id = start_address / BLOCK_SIZE;
-	remaining = nbytes;
-	offset = start_address % BLOCK_SIZE;
-	char *temp_buffer = malloc(BLOCK_SIZE);
-	
-	int bytes_completed = 0;
-	while( remaining != 0){
-		// Want to whole block (possibly after offset)
-		if ((remaining + offset) >= BLOCK_SIZE){
-			amount = BLOCK_SIZE - offset;
-		}
-		// Want to write only part of the block
-		else {
-			amount = remaining;
-		}
-		
-		// We have a partial write; read first
-		if (amount != BLOCK_SIZE) {
-			mydisk_read_block(block_id,temp_buffer);
-			// Combine into one block for writing
-			memcpy(&temp_buffer[offset], &buffer[bytes_completed], amount);
-			mydisk_write_block(block_id, temp_buffer);
-		}
-		// Full write
-		else {
-			mydisk_write_block(block_id, &buffer[bytes_completed]);
-		}
-		
-		// Update counters
-		bytes_completed += amount;
-		remaining -= amount;
-		block_id++;
-		// Offset only useful for first block
-		offset = 0;
-	}
-	free(temp_buffer);
-	 
-	return 0;
+int mydisk_write(int start_address, int nbytes, void *buffer) {
+    /* TODO: similar to read, except the partial write problem
+     * When a block is modified partially, you need to first read the block,
+     * modify the portion and then write the whole block back
+     */
+    int offset, remaining, amount, block_id;
+    int cache_hit = 0, cache_miss = 0;
+
+    // Parameter check
+    if ((start_address < 0) || (start_address + nbytes < start_address) || (start_address + nbytes >= (max_blocks * BLOCK_SIZE))) {
+        return 1;
+    }
+
+    // Init needed variables
+    block_id = start_address / BLOCK_SIZE;
+    remaining = nbytes;
+    offset = start_address % BLOCK_SIZE;
+    char *temp_buffer = malloc(BLOCK_SIZE);
+
+    int bytes_completed = 0;
+    while (remaining != 0) {
+        // Want to whole block (possibly after offset)
+        if ((remaining + offset) >= BLOCK_SIZE) {
+            amount = BLOCK_SIZE - offset;
+        }            // Want to write only part of the block
+        else {
+            amount = remaining;
+        }
+
+        // We have a partial write; read first
+        if (amount != BLOCK_SIZE) {
+            mydisk_read_block(block_id, temp_buffer);
+            // Combine into one block for writing
+            memcpy(&temp_buffer[offset], &buffer[bytes_completed], amount);
+            mydisk_write_block(block_id, temp_buffer);
+        }            // Full write
+        else {
+            mydisk_write_block(block_id, &buffer[bytes_completed]);
+        }
+
+        // Update counters
+        bytes_completed += amount;
+        remaining -= amount;
+        block_id++;
+        // Offset only useful for first block
+        offset = 0;
+    }
+    free(temp_buffer);
+
+    return 0;
 }
