267e24fc3a9181dac471ec4de8aa7047d3c7b3c1
Michael Smith <michael.smith6@mail.mcgill.ca>
2014-03-30 14:19:38 -0400

    Updated comments for ResultList

--- AI/Project/src/s260481943/ResultList.java
+++ AI/Project/src/s260481943/ResultList.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package s260481943;
 
 import halma.CCMove;
@@ -12,44 +6,65 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- *
- * @author michael
+ * Responsible for storing moves and their result from MonteCarlo.
+ * @author Michael Smith
  */
-
-
-
 public class ResultList {
     
+    // Hashmap acts as storage
     private final HashMap<CCMove, Integer> storage;
 
+    // A lock to control concurrent access
     static class theLock extends Object {
     }
     static private final theLock lockObject = new theLock();
 
+    /**
+     * Constructor creates the list
+     */
     public ResultList() {
         storage = new HashMap<>();
     }
 
+    /**
+     * Add a move to the list with its corresponding value.  If the move 
+     * already exists, the value will be added to the existing entry.
+     * @param move The move to be added or modified.
+     * @param status The value associated with the given move.
+     */
     public void addMove(CCMove move, int status) {
 
         Integer statusObj = (Integer) status;
-        // Item already exists, modify entry
+        
+        // Synchronized is used to prevent concurrent access issues
         synchronized (lockObject) {
+            // Item already exists, modify entry
             if (storage.containsKey(move)) {
                 storage.put(move, storage.get(move) + status);
-            } else {
+            } 
+            // New entry
+            else {
                 storage.put(move, statusObj);
             }
         }
     }
 
+    /**
+     * Get the best move so far.
+     * @return The move with the greatest value.
+     */
     public CCMove getBest() {
         int maxValue = Integer.MIN_VALUE;
         CCMove bestMove = null;
+        
+        // Prevent concurrent access
         synchronized (lockObject) {
+            // Get the hashmap as a set so we can work with it
             Set<Map.Entry<CCMove, Integer>> set = storage.entrySet();
+            // Check the value of each entry and compare to the max. so far
             for (Map.Entry<CCMove, Integer> map : set) {
                 if (map.getValue() > maxValue) {
+                    // This value is better; we have a new best move
                     maxValue = map.getValue();
                     bestMove = map.getKey();
                 }
