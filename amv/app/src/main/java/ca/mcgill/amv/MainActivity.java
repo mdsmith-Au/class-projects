972d4c490e12b73d76e409531674b220423de2c6
Nikolaos Bukas <nikolaos.bukas@mail.mcgill.ca>
2015-02-21 16:07:32 -0500

    A LOT OF WORK DONE

--- amv/app/src/main/java/ca/mcgill/amv/MainActivity.java
+++ amv/app/src/main/java/ca/mcgill/amv/MainActivity.java
@@ -9,8 +9,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
 public class MainActivity extends ActionBarActivity {
+    public static boolean isAdditionMode = false;
+    public static boolean isScalarProductMode = false;
+    public static boolean isCrossProductMode = false;
+    public static boolean inPolarMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +44,212 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void setVectorAddition(View view) {
+        EditText vecOneX = (EditText) findViewById(R.id.text_vector_1_x);
+        EditText vecOneY = (EditText) findViewById(R.id.text_vector_1_y);
+        EditText vecTwoX = (EditText) findViewById(R.id.text_vector_2_x);
+        EditText vecTwoY = (EditText) findViewById(R.id.text_vector_2_y);
+        EditText vecThreeX = (EditText) findViewById(R.id.text_vector_3_x);
+        EditText vecThreeY = (EditText) findViewById(R.id.text_vector_3_y);
+
+        Button calculateButton = (Button) findViewById(R.id.button_calculate);
+
+        TextView vectorOneTextView = (TextView) findViewById(R.id.textView_Vector1);
+        TextView vectorTwoTextView = (TextView) findViewById(R.id.textView_Vector2);
+        TextView vectorThreeTextView = (TextView) findViewById(R.id.textView_Vector3);
+        TextView results = (TextView) findViewById(R.id.text_result);
+
+        vecOneX.setVisibility(View.VISIBLE);
+        vecOneY.setVisibility(View.VISIBLE);
+        vecTwoX.setVisibility(View.VISIBLE);
+        vecTwoY.setVisibility(View.VISIBLE);
+        vecThreeX.setVisibility(View.VISIBLE);
+        vecThreeY.setVisibility(View.VISIBLE);
+        vecThreeX.setText("");
+        vecThreeY.setText("");
+
+        calculateButton.setVisibility(View.VISIBLE);
+
+        vectorOneTextView.setVisibility(View.VISIBLE);
+        vectorTwoTextView.setVisibility(View.VISIBLE);
+        vectorThreeTextView.setVisibility(View.VISIBLE);
+        results.setVisibility(View.VISIBLE);
+        results.setText("");
+
+        setAdditionMode();
+    }
+
+    public void setScalarProduct(View view) {
+        EditText vecOneX = (EditText) findViewById(R.id.text_vector_1_x);
+        EditText vecOneY = (EditText) findViewById(R.id.text_vector_1_y);
+        EditText vecTwoX = (EditText) findViewById(R.id.text_vector_2_x);
+        EditText vecTwoY = (EditText) findViewById(R.id.text_vector_2_y);
+        EditText vecThreeX = (EditText) findViewById(R.id.text_vector_3_x);
+        EditText vecThreeY = (EditText) findViewById(R.id.text_vector_3_y);
+
+        Button calculateButton = (Button) findViewById(R.id.button_calculate);
+
+        TextView vectorOneTextView = (TextView) findViewById(R.id.textView_Vector1);
+        TextView vectorTwoTextView = (TextView) findViewById(R.id.textView_Vector2);
+        TextView vectorThreeTextView = (TextView) findViewById(R.id.textView_Vector3);
+        TextView results = (TextView) findViewById(R.id.text_result);
+
+        vecOneX.setVisibility(View.VISIBLE);
+        vecOneY.setVisibility(View.VISIBLE);
+        vecTwoX.setVisibility(View.VISIBLE);
+        vecTwoY.setVisibility(View.VISIBLE);
+        vecThreeX.setVisibility(View.INVISIBLE);
+        vecThreeY.setVisibility(View.INVISIBLE);
+
+        calculateButton.setVisibility(View.VISIBLE);
+
+        vectorOneTextView.setVisibility(View.VISIBLE);
+        vectorTwoTextView.setVisibility(View.VISIBLE);
+        vectorThreeTextView.setVisibility(View.INVISIBLE);
+        results.setVisibility(View.VISIBLE);
+        results.setText("");
+
+        setScalarProductMode();
+    }
+
+    public void setCrossProduct(View view) {
+        EditText vecOneX = (EditText) findViewById(R.id.text_vector_1_x);
+        EditText vecOneY = (EditText) findViewById(R.id.text_vector_1_y);
+        EditText vecTwoX = (EditText) findViewById(R.id.text_vector_2_x);
+        EditText vecTwoY = (EditText) findViewById(R.id.text_vector_2_y);
+        EditText vecThreeX = (EditText) findViewById(R.id.text_vector_3_x);
+        EditText vecThreeY = (EditText) findViewById(R.id.text_vector_3_y);
+
+        Button calculateButton = (Button) findViewById(R.id.button_calculate);
+
+        TextView vectorOneTextView = (TextView) findViewById(R.id.textView_Vector1);
+        TextView vectorTwoTextView = (TextView) findViewById(R.id.textView_Vector2);
+        TextView vectorThreeTextView = (TextView) findViewById(R.id.textView_Vector3);
+        TextView results = (TextView) findViewById(R.id.text_result);
+
+        vecOneX.setVisibility(View.VISIBLE);
+        vecOneY.setVisibility(View.VISIBLE);
+        vecTwoX.setVisibility(View.VISIBLE);
+        vecTwoY.setVisibility(View.VISIBLE);
+        vecThreeX.setVisibility(View.INVISIBLE);
+        vecThreeY.setVisibility(View.INVISIBLE);
+
+        calculateButton.setVisibility(View.VISIBLE);
+
+        vectorOneTextView.setVisibility(View.VISIBLE);
+        vectorTwoTextView.setVisibility(View.VISIBLE);
+        vectorThreeTextView.setVisibility(View.INVISIBLE);
+        results.setVisibility(View.VISIBLE);
+        results.setText("");
+
+        setCrossProductMode();
+    }
+
+    public void runCalculate(View view) {
+        EditText vec1XText = (EditText) findViewById(R.id.text_vector_1_x);
+        EditText vec1YText = (EditText) findViewById(R.id.text_vector_1_y);
+        EditText vec2XText = (EditText) findViewById(R.id.text_vector_2_x);
+        EditText vec2YText = (EditText) findViewById(R.id.text_vector_2_y);
+        EditText vec3XText = (EditText) findViewById(R.id.text_vector_3_x);
+        EditText vec3YText = (EditText) findViewById(R.id.text_vector_3_y);
+
+        TextView vec1View = (TextView) findViewById(R.id.textView_Vector1);
+        TextView vec2View = (TextView) findViewById(R.id.textView_Vector2);
+        TextView vec3View = (TextView) findViewById(R.id.textView_Vector3);
+        TextView results = (TextView) findViewById(R.id.text_result);
+
+        Button calculateButton = (Button) findViewById(R.id.button_calculate);
+
+        String result = calculate(vec1XText.getText().toString(), vec1YText.getText().toString(),
+                                  vec2XText.getText().toString(), vec2YText.getText().toString(),
+                                  vec3XText.getText().toString(), vec3YText.getText().toString());
+        results.setText(result);
+    }
+
     public static double[] vectorAddition(double[] vec1, double[] vec2, double[] vec3) {
-        return new double[] {0.0, 0.0};
+        double[] results = new double[vec1.length];
+
+        results[0] = vec1[0] + vec2[0] + vec3[0];
+        results[1] = vec1[1] + vec2[1] + vec3[1];
+
+        return results;
+    }
+
+    public static double scalarVectorProduct(double[] vec1, double[] vec2) {
+        return (vec1[0] * vec2[0]) + (vec1[1] * vec2[1]);
+    }
+
+    public static double crossVectorProduct(double[] vec1, double[] vec2) {
+        return (vec1[0] * vec2[1]) - (vec1[1] * vec2[0]);
+    }
+
+    public static double[] convertPolarToCartesianCoordinates(double[] polarCoord) {
+        double[] results = new double[2];
+
+        results[0] = polarCoord[0] * Math.cos(Math.toRadians(polarCoord[1]));
+        results[1] = polarCoord[0] * Math.sin(Math.toRadians(polarCoord[1]));
+
+        return results;
+    }
+
+    public static void setAdditionMode() {
+        isAdditionMode = true;
+        isScalarProductMode = false;
+        isCrossProductMode = false;
+    }
+
+    public static void setScalarProductMode() {
+        isAdditionMode = false;
+        isScalarProductMode = true;
+        isCrossProductMode = false;
+    }
+
+    public static void setCrossProductMode() {
+        isAdditionMode = false;
+        isScalarProductMode = false;
+        isCrossProductMode = true;
+    }
+
+    public static void setPolarMode() {
+        inPolarMode = true;
+    }
+
+    public static void setCartesianMode() {
+        inPolarMode = false;
+    }
+
+    public static String calculate(String vec1X, String vec1Y, String vec2X, String vec2Y, String vec3X, String vec3Y) {
+        if(!inPolarMode) {
+            if (isAdditionMode) {
+                double[] vec1 = new double[]{0.0, 0.0};
+                double[] vec2 = new double[]{0.0, 0.0};
+                double[] vec3 = new double[]{0.0, 0.0};
+
+                if (!vec1X.isEmpty() && !vec1Y.isEmpty()) {
+                    vec1 = new double[]{Double.parseDouble(vec1X), Double.parseDouble(vec1Y)};
+                }
+                if (!vec2X.isEmpty() && !vec2Y.isEmpty()) {
+                    vec2 = new double[]{Double.parseDouble(vec2X), Double.parseDouble(vec2Y)};
+                }
+                if (!vec3X.isEmpty() && !vec3Y.isEmpty()) {
+                    vec3 = new double[]{Double.parseDouble(vec3X), Double.parseDouble(vec3Y)};
+                }
+
+                double[] result = vectorAddition(vec1, vec2, vec3);
+                return "{" + result[0] + ", " + result[1] + "}";
+            } else if (isScalarProductMode) {
+                double[] vec1 = new double[]{Double.parseDouble(vec1X), Double.parseDouble(vec1Y)};
+                double[] vec2 = new double[]{Double.parseDouble(vec2X), Double.parseDouble(vec2Y)};
+
+                return Double.toString(scalarVectorProduct(vec1, vec2));
+            } else {
+                double[] vec1 = new double[]{Double.parseDouble(vec1X), Double.parseDouble(vec1Y)};
+                double[] vec2 = new double[]{Double.parseDouble(vec2X), Double.parseDouble(vec2Y)};
+
+                return Double.toString(crossVectorProduct(vec1, vec2));
+            }
+        }
+
+        return "";
     }
 }
--- amv/app/src/test/java/ca/mcgill/amv/MainActivityTest.java
+++ amv/app/src/test/java/ca/mcgill/amv/MainActivityTest.java
@@ -14,10 +14,140 @@ public class MainActivityTest {
 
     @Test
     public void testOneValueForAddition() {
-        double[] result = MainActivity.vectorAddition(new double[] {1.0, 2.0}, new double[]{}, new double[]{});
+        double[] result = MainActivity.vectorAddition(new double[] {1.0, 2.0}, new double[] {0.0, 0.0}, new double[] {0.0, 0.0});
 
         assertEquals(result.length, 2);
         assertEquals(result[0], 1.0, 0.1);
         assertEquals(result[1], 2.0, 0.1);
+    }
+
+    @Test
+    public void testTwoValuesForAddition() {
+        double[] result = MainActivity.vectorAddition(new double[] {1.0, 2.0}, new double[] {3.0, 4.0}, new double[] {0.0, 0.0});
+
+        assertEquals(result.length, 2);
+        assertEquals(result[0], 4.0, 0.1);
+        assertEquals(result[1], 6.0, 0.1);
+    }
+
+    @Test
+    public void testThreeValuesForAddition() {
+        double[] result = MainActivity.vectorAddition(new double[] {-1.0, -2.0}, new double[] {1.0, 3.0}, new double[] {2.0, 4.0});
+
+        assertEquals(result.length, 2);
+        assertEquals(result[0], 2.0, 0.1);
+        assertEquals(result[1], 5.0, 0.1);
+    }
+
+    @Test
+    public void testScalarProduct() {
+        double result = MainActivity.scalarVectorProduct(new double[] {-1.0, 2.0}, new double[] {3.0, 7.5});
+
+        assertEquals(result, 12, 0.1);
+    }
+
+    @Test
+    public void testCrossProduct() {
+        double result = MainActivity.crossVectorProduct(new double[] {-1.0, 2.0}, new double[] {3.0, 7.5});
+
+        assertEquals(result, -13.5, 0.1);
+    }
+
+    @Test
+    public void testConvertPolarToCartesianCoordinates() {
+        double[] result = MainActivity.convertPolarToCartesianCoordinates(new double[] {-5.0, -50});
+
+        assertEquals(result[0], -3.214, 0.1);
+        assertEquals(result[1], 3.83, 0.1);
+    }
+
+    @Test
+    public void testAdditionModeSet() {
+        MainActivity.setAdditionMode();
+
+        assertTrue(MainActivity.isAdditionMode);
+        assertFalse(MainActivity.isScalarProductMode);
+        assertFalse(MainActivity.isCrossProductMode);
+    }
+
+    @Test
+    public void testScalarProductMode() {
+        MainActivity.setScalarProductMode();
+
+        assertFalse(MainActivity.isAdditionMode);
+        assertTrue(MainActivity.isScalarProductMode);
+        assertFalse(MainActivity.isCrossProductMode);
+    }
+
+    @Test
+    public void testCrossProductMode() {
+        MainActivity.setCrossProductMode();
+
+        assertFalse(MainActivity.isAdditionMode);
+        assertFalse(MainActivity.isScalarProductMode);
+        assertTrue(MainActivity.isCrossProductMode);
+    }
+
+    @Test
+     public void testPolarModeSet() {
+        MainActivity.setPolarMode();
+
+        assertTrue(MainActivity.inPolarMode);
+    }
+
+    @Test
+    public void testCartesianMode() {
+        MainActivity.setPolarMode();
+        MainActivity.setCartesianMode();
+
+        assertFalse(MainActivity.inPolarMode);
+    }
+
+    @Test
+    public void testVectorAdditionForOneCartesianValue() {
+        MainActivity.setCartesianMode();
+        MainActivity.setAdditionMode();
+        String result = MainActivity.calculate("1.0", "2.0", "", "", "", "");
+
+        assertEquals("{1.0, 2.0}", result);
+    }
+
+    @Test
+    public void testVectorAdditionForTwoCartesianValue() {
+        MainActivity.setCartesianMode();
+        MainActivity.setAdditionMode();
+        String result = MainActivity.calculate("1.0", "2.0", "3.0", "4.0", "", "");
+
+        assertEquals("{4.0, 6.0}", result);
+    }
+
+    @Test
+    public void testVectorAdditionForThreeCartesianValue() {
+        MainActivity.setCartesianMode();
+        MainActivity.setAdditionMode();
+
+        String result = MainActivity.calculate("-1.0", "-2.0", "1.0", "3.0", "2.0", "4.0");
+
+        assertEquals("{2.0, 5.0}", result);
+    }
+
+    @Test
+    public void testScalarProductForCartesianVectors() {
+        MainActivity.setCartesianMode();
+        MainActivity.setScalarProductMode();
+
+        String result = MainActivity.calculate("-1.0", "2.0", "3.0", "7.5", "", "");
+
+        assertEquals("12.0", result);
+    }
+
+    @Test
+    public void testCrossProductForCartesianVectors() {
+        MainActivity.setCartesianMode();
+        MainActivity.setCrossProductMode();
+
+        String result = MainActivity.calculate("-1.0", "2.0", "3.0", "7.5", "", "");
+
+        assertEquals("-13.5", result);
     }
 }
